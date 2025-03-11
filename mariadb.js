5148cec25de2a9d4d86acb4c8a7e35f802cc338f
양시은 <mn07020@gmail.com>
2025-03-11 13:28:40 +0900

    세번째 커밋 : DB 연결

---
+++ mariadb.js
@@ -0,0 +1,12 @@
+const mariadb = require('mysql2');
+
+const connection = mariadb.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'Bookshop',
+    port: 3307,
+    dateStrings : true
+});
+
+module.exports = connection
