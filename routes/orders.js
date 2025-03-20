aea5e1281fd9214c59fccb1fdb848a793934381c
양시은 <mn07020@gmail.com>
2025-03-20 13:16:58 +0900

    열한번째 커밋 : 주문 내역 조회 기능 구현

--- routes/orders.js
+++ routes/orders.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const {
+    order,
+    getOrders,
+    getOrderDetail
+} = require('../controller/OrderController')
+
 router.use(express.json());
 
-//주문하기
-router.post('/', (req, res) => {
-    res.json('주문 하기');
-});
-
-//주문 목록 조회
-router.get('/', (req, res) => {
-    res.json('주문 목록 조회');
-});
-
-
-//비밀번호 초기화 요청
-router.get('/:id',(req, res) => {
-    res.json('주문 상세 상품 조회');
-});
-
+router.post('/', order);//주문하기
+router.get('/', getOrders);//주문 목록 조회
+router.get('/:id', getOrderDetail);//주문 상세 조회
 
 module.exports = router
